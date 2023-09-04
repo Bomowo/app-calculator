ac03e8593889bd7ebeb52e14132bbe3a3cf40476
Bomowo <oeccc25@yahoo.com>
2023-09-04 18:43:22 +0300

    Refactor code to use bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -7,41 +7,41 @@ function App() {
     <div>
       <div className="container">
         <div className="row">
-          <h1 className="six columns">calc</h1>
-          <p className="six columns">THEME <span>switch with 3 cases</span></p>
+          <h1 className="col">calc</h1>
+          <p className="col">THEME <span>switch with 3 cases</span></p>
         </div>
         <div className="row">
-          <input className="twelve columns" type="number" />
+          <input className="col" type="number" />
         </div>
       </div>
       <div className="container"> 
         <div className="row">
-          <button className="three columns">7</button>
-          <button className="three columns">8</button>
-          <button className="three columns">9</button>
-          <button className="three columns">DEL</button>
+          <button className="col">7</button>
+          <button className="col">8</button>
+          <button className="col">9</button>
+          <button className="col">DEL</button>
         </div>
         <div className="row">
-          <button className="three columns">4</button>
-          <button className="three columns">5</button>
-          <button className="three columns">6</button>
-          <button className="three columns">+</button>
+          <button className="col">4</button>
+          <button className="col">5</button>
+          <button className="col">6</button>
+          <button className="col">+</button>
         </div>
         <div className="row">
-          <button className="three columns">1</button>
-          <button className="three columns">2</button>
-          <button className="three columns">3</button>
-          <button className="three columns">-</button>
+          <button className="col">1</button>
+          <button className="col">2</button>
+          <button className="col">3</button>
+          <button className="col">-</button>
         </div>
         <div className="row">
-          <button className="three columns">.</button>
-          <button className="three columns">0</button>
-          <button className="three columns">/</button>
-          <button className="three columns">x</button>
+          <button className="col">.</button>
+          <button className="col">0</button>
+          <button className="col">/</button>
+          <button className="col">x</button>
         </div>
         <div className="row">
-          <button className="six columns">RESET</button>
-          <button className="six columns">=</button>
+          <button className="col">RESET</button>
+          <button className="col">=</button>
         </div>
       </div>
     </div>
